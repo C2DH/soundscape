1413af1aaea3b8da86d044b144a13001d1cc43df
donsiamese <donsiamese@gmail.com>
2025-08-12 09:22:41 +0200

    glsl settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import glsl from "vite-plugin-glsl";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-})
+  plugins: [react(), glsl(), tailwindcss()],
+  resolve: {
+    alias: {
+      "three/addons": "three/examples/jsm",
+      "three/examples/jsm": "three/examples/jsm",
+    },
+  },
+  assetsInclude: ["**/*.glsl"],
+});
