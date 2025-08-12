9399d4476fd9e7c497010afc4fd57cb07f45777e
Daniele Guido <1181642+danieleguido@users.noreply.github.com>
2025-08-12 11:17:47 +0200

    Create types.d.ts

---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+export type GeoPoint = {
+  id?: string // Optional ID for the point
+  name?: string // Optional name for the point
+  description?: string // Optional description for the point
+  lat: number // in degrees
+  lon: number // in degrees
+  color?: string
+}
