ba4ec45edf4d595db8a930711bb2fa7cdb690261
Daniele Guido <1181642+danieleguido@users.noreply.github.com>
2025-08-12 11:17:45 +0200

    add useWorldStore

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import type { GeoPoint } from './types'
 
 export const useStore = create<{
   currentParamItemId: string | undefined
@@ -7,4 +8,18 @@ export const useStore = create<{
   currentParamItemId: undefined,
   setCurrentParamItemId: (itemId: string | undefined) =>
     set(() => ({ currentParamItemId: itemId })),
+}))
+
+export const useWorldStore = create<{
+  geoPoints: GeoPoint[]
+  // Function to set geoPoints, which can be used to update the state
+  // This function can be used in components to update the list of geoPoints
+  setGeoPoints: (points: GeoPoint[]) => void
+  highlightedPoint?: GeoPoint
+  setHighlightedPoint: (point: GeoPoint | undefined) => void
+}>((set) => ({
+  geoPoints: [],
+  setGeoPoints: (geoPoints) => set(() => ({ geoPoints })),
+  highlightedPoint: undefined,
+  setHighlightedPoint: (point) => set(() => ({ highlightedPoint: point })),
 }))
