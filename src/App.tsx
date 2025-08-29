42cf5faad16afe55e4c46185993bd62d1d1f628b
donsiamese <donsiamese@gmail.com>
2025-08-29 13:56:24 +0200

    Update App.tsx
    add Footer / geo positions

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,19 @@ import SoundScape from './components/SoundScape';
 import frequenceListDenmark from './assets/frequencies_denmark.json';
 import { amplifyLists } from './components/SoundScapePlayer';
 import { useThemeStore } from './store';
+import Footer from './components/Footer';
+import Button from './components/Button';
 
 const DenmarkScene: React.FC = () => {
   // ⬇️ This is basically your Storybook args
   const args = {
     lists: amplifyLists(frequenceListDenmark, 0.5),
-    showWireframe: true,
   };
 
   const color = useThemeStore((s) => s.colors['--light']);
 
   return (
-    <Canvas shadows camera={{ position: [50, 50, 25], fov: 50 }}>
+    <Canvas shadows camera={{ position: [100, 100, 50], fov: 50 }}>
       <ambientLight intensity={1} />
       <directionalLight castShadow intensity={1} position={[-5, 3, 0]} />
       <directionalLight intensity={3} position={[3, 3, 0]} />
@@ -52,33 +53,20 @@ function App() {
   }, [refreshFromCSS]);
 
   const geoPoints = [
-    { lat: 48.8566, lon: 2.3522, color: 'red', id: 'paris' }, // Paris
-    { lat: 40.7128, lon: -74.006, color: 'blue', id: 'new-york' }, // New York
-    { lat: -33.8688, lon: 151.2093, color: 'green', id: 'sydney' }, // Sydney
-    { lat: 51.5074, lon: -0.1278, color: 'purple', id: 'london' }, // London
-    { lat: 35.6895, lon: 139.6917, color: 'orange', id: 'tokyo' }, // Tokyo
-    { lat: 55.7558, lon: 37.6173, color: 'pink', id: 'moscow' }, // Moscow
-    { lat: 34.0522, lon: -118.2437, color: 'yellow', id: 'los-angeles' }, // Los Angeles
-    { lat: 39.9042, lon: 116.4074, color: 'cyan', id: 'beijing' }, // Beijing
-    { lat: -23.5505, lon: -46.6333, color: 'brown', id: 'sao-paulo' }, // Sao Paulo
-    { lat: 1.3521, lon: 103.8198, color: 'grey', id: 'singapore' }, // Singapore
-    { lat: 37.7749, lon: -122.4194, color: 'teal', id: 'san-francisco' }, // San Francisco
-    { lat: 55.6761, lon: 12.5683, color: 'lime', id: 'copenhagen' }, // Copenhagen
-    { lat: 52.52, lon: 13.405, color: 'maroon', id: 'berlin' }, // Berlin
-    { lat: 41.9028, lon: 12.4964, color: 'navy', id: 'rome' }, // Rome
-    { lat: 37.9838, lon: 23.7275, color: 'olive', id: 'athens' }, // Athens
-    { lat: 19.4326, lon: -99.1332, color: 'coral', id: 'mexico-city' }, // Mexico City
-    { lat: 59.3293, lon: 18.0686, color: 'gold', id: 'stockholm' }, // Stockholm
+    { id: 'denmark', name: 'Denmark', lat: 56.2639, lon: 9.5018 },
+    { id: 'sweden', name: 'Sweden', lat: 60.1282, lon: 18.6435 },
+    { id: 'norway', name: 'Norway', lat: 60.472, lon: 8.4689 },
+    { id: 'luxembourg', name: 'Luxembourg', lat: 49.6118, lon: 6.1319 },
+    { id: 'finland', name: 'Finland', lat: 61.9241, lon: 25.7482 },
+    { id: 'iceland', name: 'Iceland', lat: 64.9631, lon: -19.0208 },
+    { id: 'italy', name: 'Italy', lat: 41.8719, lon: 12.5674 },
+    { id: 'spain', name: 'Spain', lat: 40.4637, lon: -3.7492 },
+    { id: 'portugal', name: 'Portugal', lat: 39.3999, lon: -8.2245 },
+    { id: 'belgium', name: 'Belgium', lat: 50.5039, lon: 4.4699 },
+    { id: 'netherlands', name: 'Netherlands', lat: 52.3676, lon: 4.9041 },
   ];
   return (
     <>
-      <Header />
-      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        {/* Future Canvas component will go here */}
-        <div className="h-full w-full flex items-center justify-cente">
-          <DenmarkScene />
-        </div>
-      </Modal>
       <LocationManager />
       <Routes>
         <Route index element={null} />
@@ -86,12 +74,12 @@ function App() {
       </Routes>
 
       <main className="h-full w-full">
-        <button
-          className="absolute bottom-4 left-4 px-4 py-2 bg-blue-600 text-white rounded-lg z-10"
+        <Header />
+        <Button
+          label="Open Modal"
+          className="absolute bottom-20 left-[calc(50%-3rem)] z-10"
           onClick={() => setIsOpen(true)}
-        >
-          Open Modal
-        </button>
+        />
         <Canvas shadows camera={{ position: [5, 5, 5], fov: 100 }}>
           <ambientLight intensity={0.4} />
           <directionalLight
@@ -102,11 +90,21 @@ function App() {
             shadow-mapSize-height={1024}
           />
 
-          <World geoPoints={geoPoints} radius={5} />
+          <World geoPoints={geoPoints} radius={5.3} />
 
-          <OrbitControls enablePan={false} />
+          <OrbitControls enablePan={false} minDistance={6} maxDistance={10} />
         </Canvas>
+        <Footer />
       </main>
+
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <div className="h-full w-full flex items-center justify-cente">
+          <p className="big-text font-medium tracking-[-0.06em] uppercase absolute w-screen text-center top-[10%] text-[12vw] opacity-20">
+            Denmark
+          </p>
+          <DenmarkScene />
+        </div>
+      </Modal>
     </>
   );
 }
