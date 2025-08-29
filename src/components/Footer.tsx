e7f892e14d22108b1e1e28f03b3edae0604b698b
donsiamese <donsiamese@gmail.com>
2025-08-29 16:28:21 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,11 +11,10 @@ const Footer: React.FC = () => {
           <LogoUniLinkoping width={110} />
         </div>
 
-        {/* Right side: Copyright */}
-        <div className="text-[0.8rem] text-[rgba(var(--light),1)] my-2">
+        <span className="text-[0.8rem] text-[rgba(var(--light),1)] my-2">
           © {new Date().getFullYear()} University of Luxembourg and Linköping University. All
           rights reserved.
-        </div>
+        </span>
       </div>
     </footer>
   );
