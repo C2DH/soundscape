1b0306d280bfc4d76786dd8d5f5b37bae59f59bd
donsiamese <donsiamese@gmail.com>
2025-08-29 16:22:45 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,16 @@ import LogoUniLinkoping from './svg/LogoUniLinkoping';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="fixed bottom-2 left-0 w-full bg-transparent">
-      <div className="max-w-10xl mx-auto flex items-center justify-between px-6 py-3">
+    <footer className="fixed bottom-2 left-0 w-full bg-transparent gap-2">
+      <div className="max-w-10xl mx-auto flex items-center justify-between px-2 md:px-8 py-3">
         {/* Left side: Logos */}
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4 m-2">
           <LogoUniLux width={120} />
           <LogoUniLinkoping width={110} />
         </div>
 
         {/* Right side: Copyright */}
-        <div className="text-[0.8rem] text-[rgba(var(--light),1)]">
+        <div className="text-[0.8rem] text-[rgba(var(--light),1)] my-2">
           © {new Date().getFullYear()} University of Luxembourg and Linköping University. All
           rights reserved.
         </div>
