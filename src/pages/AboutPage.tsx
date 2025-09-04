266fa4cd2c486d884ae98e4654a6b8372e1f0c9d
donsiamese <donsiamese@gmail.com>
2025-09-04 11:48:53 +0200

    Create AboutPage.tsx

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+interface AboutData {
+  title: string;
+  content: string;
+}
+
+const About: React.FC = () => {
+  const [data, setData] = useState<AboutData | null>(null);
+
+  useEffect(() => {
+    import('../assets/data/pages/about.json').then((json) => {
+      setData(json as AboutData);
+    });
+  }, []);
+
+  if (!data) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="flex flex-col py-[7rem] items-center justify-center w-screen h-screen absolute top-0 left-0 z-2">
+      <div className="w-full max-w-3xl p-6 overflow-y-auto max-h-screen">
+        <h1 className="text-3xl font-bold mb-4">{data.title}</h1>
+        <p className="text-md font-medium leading-relaxed whitespace-pre-line">{data.content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
