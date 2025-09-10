2296d7ed247bf2a48a94ea607e3b06a4fd7f398b
donsiamese <donsiamese@gmail.com>
2025-09-10 17:25:36 +0200

    add scene manager

---
+++ src/components/Scene.tsx
@@ -0,0 +1,43 @@
+import SoundScape from './SoundScape';
+import { amplifyLists } from './SoundScapePlayer';
+import { OrbitControls, Grid } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { useThemeStore } from '../store';
+
+interface SceneProps {
+  landscapeData: any;
+}
+
+const Scene: React.FC<SceneProps> = ({ landscapeData }) => {
+  // ⬇️ This is basically your Storybook args
+  const args = {
+    lists: amplifyLists(landscapeData, 0.6),
+  };
+
+  const color = useThemeStore((s) => s.colors['--light']);
+
+  return (
+    <div className="w-full h-full">
+      <p className="big-text font-medium tracking-[-0.06em] uppercase absolute top-[10%] left-0 w-full flex flex-col items-center text-[12vw] opacity-20">
+        Sweden
+      </p>
+      <Canvas shadows camera={{ position: [100, 100, 50], fov: 50 }}>
+        <OrbitControls />
+        <group>
+          <SoundScape {...args} />
+          <Grid
+            args={[160, 160]}
+            cellSize={5}
+            cellColor={color}
+            sectionSize={80}
+            sectionColor={color}
+            fadeDistance={200}
+            fadeStrength={2}
+          />
+        </group>
+      </Canvas>
+    </div>
+  );
+};
+
+export default Scene;
---
+++ src/components/SceneManager.tsx
@@ -0,0 +1,34 @@
+import Modal from './Modal';
+import { useEffect } from 'react';
+import { useThemeStore, useModalStore } from '../store';
+import AudioControls from './AudioControls';
+import Scene from './Scene';
+import landscapeData from '../../src/assets/data/vis/sweden.json';
+
+const SceneManager = () => {
+  const { isOpenModal, closeModal } = useModalStore();
+  const refreshFromCSS = useThemeStore((s) => s.refreshFromCSS);
+
+  useEffect(() => {
+    refreshFromCSS();
+  }, [refreshFromCSS]);
+
+  return (
+    <div className="Scene absolute h-full w-full">
+      <Modal isOpen={isOpenModal} onClose={closeModal}>
+        {isOpenModal ? (
+          <AudioControls
+            onNextVis={() => {}}
+            onPrevVis={() => {}}
+            onNextCountry={() => {}}
+            onPrevCountry={() => {}}
+            src="/audio/Denmark.mp3"
+          />
+        ) : null}
+        <Scene landscapeData={landscapeData} />
+      </Modal>
+    </div>
+  );
+};
+
+export default SceneManager;
