3f5888df8cbe5b863ab56c2b1b2b80254326c191
donsiamese <donsiamese@gmail.com>
2025-09-12 12:22:08 +0200

    add storybook version

--- src/components/SoundLines.stories.tsx
+++ src/components/SoundLines.stories.tsx
@@ -1,9 +1,13 @@
 // SoundLines.stories.tsx
-import type { Meta, StoryObj } from '@storybook/react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import SoundLines from './SoundLines'
+import type { Meta, StoryObj } from '@storybook/react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import SoundLines from './SoundLines';
+import { Vector3 } from 'three';
 
+const mockLines = Array.from({ length: 10 }, (_, t) =>
+  Array.from({ length: 50 }, (_, i) => new Vector3(i * 0.1, Math.sin(i * 0.1 + t), 0))
+);
 const meta: Meta<typeof SoundLines> = {
   title: '3D/SoundLines',
   component: SoundLines,
@@ -11,22 +15,24 @@ const meta: Meta<typeof SoundLines> = {
     layout: 'fullscreen',
   },
   tags: ['autodocs'],
-}
-export default meta
+};
+export default meta;
 
-type Story = StoryObj<typeof SoundLines>
+type Story = StoryObj<typeof SoundLines>;
 
 export const Default: Story = {
   args: {
+    lines: mockLines,
+    lineIdx: 5,
   },
-  render: () => (
+  render: (args) => (
     <div style={{ width: '100%', height: 600, background: 'grey' }}>
       <Canvas camera={{ position: [25, 10, 25], fov: 50 }}>
         <ambientLight />
         <directionalLight position={[10, 10, 5]} />
-        <SoundLines  />
+        <SoundLines {...args} />
         <OrbitControls />
       </Canvas>
     </div>
   ),
-}
+};
