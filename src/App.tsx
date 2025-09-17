c207445de2605b3bf463da7d8fee47a2b7cb18b9
donsiamese <donsiamese@gmail.com>
2025-09-17 17:26:10 +0200

    Add TeamMember component and team section
    Introduced a reusable TeamMember component with associated styles and team member images. Updated ContactPage to display a team section using the new component. Minor UI tweaks in App.tsx and index.css for improved presentation.

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ function App() {
         >
           {path === '/' ? (
             <div className="absolute w-screen h-auto top-[40vh]  text-center flex flex-col justify-center items-center z-30">
-              <h1 className="text-4xl font-semibold max-w-[700px] mb-8">
+              <h1 className="text-4xl font-semibold max-w-[700px] mb-8 uppercase">
                 The world's most complete collection of warning sounds
               </h1>
               <Button link="/overview" label="Dicover Soundscapes" className="top-4 left-4" />
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import TeamMember from '../components/TeamMember';
 
 interface ContactPage {
   title: string;
@@ -19,13 +20,38 @@ const ContactPage: React.FC = () => {
   }
 
   return (
-    <div className="content w-screen h-screen flex items-center justify-center absolute top-0 left-0 z-2">
-      <div className="wrapper  w-screen   h-[70vh] overflow-auto flex flex-col items-center ">
-        <div className="max-w-3xl px-4">
-          <h1 className="text-3xl font-bold">{data.title}</h1>
-          <p className="text-md font-medium leading-relaxed whitespace-pre-line mt-4">
-            {data.content}
-          </p>
+    <div className="content w-screen h-screen flex items-center  justify-center absolute top-0 left-0 z-2">
+      <div className="wrapper  w-screen  pb-[2rem] h-[75vh] overflow-auto flex flex-col items-center ">
+        <div className=" flex flex-col align-center px-4">
+          <h1 className="text-3xl font-bold">Team</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-2 mt-8">
+            <TeamMember
+              image="/img/Marie.jpg"
+              title="Marie CRONQVIST (PI)"
+              subtitle="Professor of Modern History,
+      Linköping University, Sweden"
+              href="https://liu.se/en/employee/marcr79"
+            />
+            <TeamMember
+              image="/img/Johan.jpg"
+              title="Jonas LINDSTRÖM"
+              subtitle="Ph D and researcher in Media and Communication Studies and Digital Humanities, and affiliate member of the Meta Lab, Harvard University."
+              href="https://mlml.io/m/johan-malmstedt/"
+            />
+            <TeamMember
+              image="/img/Daniele.jpg"
+              title="Daniele Guido"
+              subtitle="Lead Designer"
+              href="https://www.uni.lu/c2dh-en/people/daniele-guido/"
+            />
+            <TeamMember
+              className="md:col-start-0 col-start-0"
+              image="/img/Kirill.jpg"
+              title="Kirill MITSUROV"
+              subtitle="User Interface Developer"
+              href="https://www.uni.lu/c2dh-en/people/kirill-mitsurov/"
+            />
+          </div>
         </div>
       </div>
     </div>
