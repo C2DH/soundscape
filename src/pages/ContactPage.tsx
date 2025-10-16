3131562a0d2f447c16cccc6b08252879eb14b75a
donsiamese <donsiamese@gmail.com>
2025-10-16 15:45:45 +0200

    Update team member image paths with SiteBasename
    Refactored image source paths for TeamMember components to use a dynamic prefix based on SiteBasename, ensuring correct asset referencing regardless of deployment base path.

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,9 +1,12 @@
 import TeamMember from '../components/TeamMember';
+import { SiteBasename } from '../constants';
 
 interface ContactPage {
   title: string;
   content: string;
 }
+
+const imagePrefixSrc = String(SiteBasename.replace(/\/\//g, '/'));
 
 const ContactPage: React.FC = () => {
   return (
@@ -13,27 +16,27 @@ const ContactPage: React.FC = () => {
           <h1 className="text-3xl font-bold text-center">Team</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-2 mt-8">
             <TeamMember
-              image="/img/Marie.jpg"
+              image={`${imagePrefixSrc}img/Marie.jpg`}
               title="Marie CRONQVIST (PI)"
               subtitle="Professor of Modern History,
       Linköping University, Sweden"
               href="https://liu.se/en/employee/marcr79"
             />
             <TeamMember
-              image="/img/Johan.jpg"
+              image={`${imagePrefixSrc}img/Johan.jpg`}
               title="Johan MALMSTEDT"
               subtitle="Ph D and researcher in Media and Communication Studies and Digital Humanities, and affiliate member of the Meta Lab, Harvard University."
               href="https://mlml.io/m/johan-malmstedt/"
             />
             <TeamMember
-              image="/img/Daniele.jpg"
+              image={`${imagePrefixSrc}img/Daniele.jpg`}
               title="Daniele Guido"
               subtitle="Developer, Lead Designer of C²DH, University of Luxembourg"
               href="https://www.uni.lu/c2dh-en/people/daniele-guido/"
             />
             <TeamMember
               className="md:col-start-0 col-start-0"
-              image="/img/Kirill.jpg"
+              image={`${imagePrefixSrc}img/Kirill.jpg`}
               title="Kirill MITSUROV"
               subtitle="Designer / Developer, User Interface Developer of C²DH, University of Luxembourg"
               href="https://www.uni.lu/c2dh-en/people/kirill-mitsurov/"
